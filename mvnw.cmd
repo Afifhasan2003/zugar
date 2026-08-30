0e54c90f97d1667487d57720ee45bd7eb671cad7
Md. Saikat Islam <saikatislam709@gmail.com>
2026-08-30 20:42:15 +0600

    project create

---
+++ mvnw.cmd
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+set "BASE_DIR=%~dp0"
+java -Dmaven.multiModuleProjectDirectory="%BASE_DIR%" -classpath "%BASE_DIR%.mvn\wrapper\maven-wrapper.jar" org.apache.maven.wrapper.MavenWrapperMain %*
+exit /b %ERRORLEVEL%
